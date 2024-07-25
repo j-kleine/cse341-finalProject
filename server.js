04c35ad65e03b47c8812f9c6f86e3e8cd605bc63
j-kleine <joschuadkleine@gmail.com>
2024-07-26 00:59:24 +0200

    attempt to fix "null" username

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ passport.deserializeUser((user, done) => {
 
 app.get('/', (req, res) => {
     console.log('Session user:', req.session.user);
-    res.send(req.session.user ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+    res.send(req.session.user ? `Logged in as ${req.session.user.username}` : "Logged Out")});
 
 app.get('/github/callback', passport.authenticate('github', {
     failureRedirect: '/api-docs'
